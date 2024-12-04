88068ca907650a9ab2ec064bd0e20e5b6fffcdd8
swimfinn <84697071+swimfinn@users.noreply.github.com>
2024-12-03 21:57:56 -0800

    impoved ux

--- src/style.js
+++ src/style.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a romantic poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">🖋️ Writing beautiful poetry for you about ${instructionsInput.value}!</div>`;
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
