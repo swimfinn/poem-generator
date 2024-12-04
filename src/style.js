8a3b27326afbf7e191df5261d59e88a659e12259
swimfinn <84697071+swimfinn@users.noreply.github.com>
2024-12-03 20:41:14 -0800

    updated context and cursor

--- src/style.js
+++ src/style.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "e520df23o0033b9f5bbe4tf4ae4a9b0f";
-    let context = "You are a professional romantic poet. Make it short and romantic in English with a translation in Spanish below in italics. The Spanish must rhyme also. Write only a 4-line poem that rhyme in basic html. Do not include the words html. Sign with SheCodes AI a the very body inside a <strong></strong> element Make sure to follow the user instructions";
+    let context = "You are a professional romantic poet. Make it short and romantic in English, add a </br> element, with a translation in Spanish below in italics. The Spanish must rhyme also. Write only a 4-line poem that rhyme and must be in basic html. Do not include the words html. Sign with SheCodes AI a the very body inside a <strong></strong> element. The poem though is not in bold. Make sure to follow the user instructions";
     let prompt = `User instructions: Generate a romantic poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
